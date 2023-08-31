91952f26086ee5c516eb5f151a82e6de245239f6
Gennadiy Zubkov <gena-zub96@mail.ru>
2023-08-31 16:41:34 +0300

    install redis

---
+++ project/project/__init__.py
@@ -0,0 +1,7 @@
+from .celery import app as celery_app
+
+__all__ = ('celery_app',)
+
+
+# password redis = VpX88w0DunCcv4AuBroo9jfihqY9vSv7
+# endpoint = redis-19170.c10.us-east-1-4.ec2.cloud.redislabs.com:19170
--- project/project/settings.py
+++ project/project/settings.py
@@ -188,3 +188,19 @@ APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
 # если задача не выполняется за 25 секунд, то она автоматически снимается,
 # можете поставить время побольше, но как правило, это сильно бьёт по производительности сервера
 APSCHEDULER_RUN_NOW_TIMEOUT = 25  # Seconds
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+import redis
+
+red = redis.Redis(
+    host='redis-19170.c10.us-east-1-4.ec2.cloud.redislabs.com',
+    port='19170',
+    password='VpX88w0DunCcv4AuBroo9jfihqY9vSv7'
+)
+# password redis = VpX88w0DunCcv4AuBroo9jfihqY9vSv7
+# endpoint = redis-19170.c10.us-east-1-4.ec2.cloud.redislabs.com:19170
